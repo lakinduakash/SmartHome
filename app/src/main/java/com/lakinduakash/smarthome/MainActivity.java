c5af0067c4373103713f04d803c06890a090ec9e
lakinduakash <lakinduakash@gmail.com>
2018-02-09 19:39:03 +0530

    adding extra functionality ui improve

--- app/src/main/java/com/lakinduakash/smarthome/MainActivity.java
+++ app/src/main/java/com/lakinduakash/smarthome/MainActivity.java
@@ -1,10 +1,13 @@
 package com.lakinduakash.smarthome;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -20,6 +23,10 @@ public class MainActivity extends AppCompatActivity {
     Button mainSwitch;
     EditText urlEdit;
     TextView textView;
+    ProgressBar progressBar;
+    SharedPreferences savedAddress;
+
+    String INET_ADD = "address";
 
     String curState = "";
     static final String[] STATES = {"/LED=ON", "/LED=OFF"};
@@ -30,18 +37,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         mainSwitch = findViewById(R.id.button);//Toggle button of the light
         urlEdit = findViewById(R.id.editText);//Url field
         textView = findViewById(R.id.textView);//Status area
+        progressBar = findViewById(R.id.progressBar);
+
+
+        //get saved url if any to edit field
+        savedAddress = getPreferences(Context.MODE_PRIVATE);
+        String addr = savedAddress.getString(INET_ADD, "http://192.168.");
+        urlEdit.setText(addr);
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
         //set onClick listener to button with toggle the states of bulb
         mainSwitch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 toggle();
+                mainSwitch.setClickable(false);
+                textView.setVisibility(View.INVISIBLE);
+                progressBar.setVisibility(View.VISIBLE);
+
             }
         });
-    }
 
+
+    }
 
     public void toggle() {
 
@@ -66,16 +94,21 @@ public class MainActivity extends AppCompatActivity {
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
+                        textView.setVisibility(View.VISIBLE);
+                        progressBar.setVisibility(View.INVISIBLE);
+                        mainSwitch.setClickable(true);
                         textView.setText(curState+response);
                     }
                 },new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 try {
+                    textView.setVisibility(View.VISIBLE);
+                    progressBar.setVisibility(View.INVISIBLE);
+                    mainSwitch.setClickable(true);
                     textView.setText("Error: " + error.networkResponse.statusCode);
-                }
-                catch (Exception e)
-                {
+
+                } catch (Exception e) {
                     textView.setText("Check Address ");
                 }
 
@@ -84,6 +117,22 @@ public class MainActivity extends AppCompatActivity {
 
         //add the request to queue.
         queue.add(stringRequest);
+
+        //save used url for future use
+        SharedPreferences.Editor editor = savedAddress.edit();
+        editor.putString(INET_ADD, urlEdit.getText().toString());
+        editor.apply();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        SharedPreferences.Editor editor = savedAddress.edit();
+        editor.putString(INET_ADD, urlEdit.getText().toString());
+        editor.apply();
+
+
     }
 
 
